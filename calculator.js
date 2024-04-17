94d01d589d8049a22439a316a4a406754b346a50
Stekpannan02 <167300643+Stekpannan02@users.noreply.github.com>
2024-04-17 11:43:04 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,17 +1,19 @@
-
-
 function calculateScore() {
     const BetygA = parseFloat(document.getElementById('BetygA').value);
 
     const SnabbaLabb = parseFloat(document.getElementById('SnabbaLabb').value);
     const SnabbaRapp = parseFloat(document.getElementById('SnabbaRapp').value);
     const SnabbaFor = parseFloat(document.getElementById('SnabbaFor').value);
-    
+
     const BrusLabb = parseFloat(document.getElementById('BrusLabb').value);
     const BrusRapp = parseFloat(document.getElementById('BrusRapp').value);
     const BrusFor = parseFloat(document.getElementById('BrusFor').value);
 
-    // Retrieve all other variables similarly
+    const TeoriFor = parseFloat(document.getElementById('TeoriFor').value);
+
+    const LabFor = parseFloat(document.getElementById('LabFor').value);
+    const LabLab = parseFloat(document.getElementById('LabLab').value);
+    const LabLars = parseFloat(document.getElementById('LabLars').value);
 
     const Snabba = SnabbaFor + SnabbaLabb * 2 + SnabbaRapp * 2;
     const Brus = BrusFor + BrusLabb * 2 + BrusRapp * 2;
